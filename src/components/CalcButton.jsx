5570432d2e1ec47da773665065a120b89269115d
chuy-zip <andreschuy03@gmail.com>
2024-05-24 16:46:08 -0600

    added 2 buttons

--- src/components/CalcButton.jsx
+++ src/components/CalcButton.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types'
-import { lengthIsCorrect, 
-         isNegative, 
-         isNumber, 
-         plusMinus, 
-         appendNumber, 
-         isOperator, 
-         add, 
-         subtract, 
-         multiply, 
-         divide } from '@scripts/calcController.js';
+import {
+    lengthIsCorrect,
+    isNegative,
+    isNumber,
+    plusMinus,
+    appendNumber,
+    isOperator,
+    add,
+    subtract,
+    multiply,
+    divide
+} from '@scripts/calcController.js';
 
 CalcButton.propTypes = {
     content: PropTypes.string.isRequired,
@@ -16,91 +18,101 @@ CalcButton.propTypes = {
     num2: PropTypes.string.isRequired,
 }
 
-function CalcButton({content, num1, num2, result}){
+function CalcButton({ content, num1, num2, result }) {
 
     const buttonConfig = {
-        'C': { 
-          class: 'options', 
-          function: () => { console.log('C button clicked'); } 
+        'C': {
+            class: 'options',
+            function: () => { console.log('C button clicked'); }
         },
-        '+/-': { 
-          class: 'options', 
-          function: () => { console.log('+/- button clicked'); } 
+        '+/-': {
+            class: 'options',
+            function: () => { console.log('+/- button clicked'); }
         },
-        '%': { 
-          class: 'options', 
-          function: () => { console.log('% button clicked'); } 
+        '%': {
+            class: 'options',
+            function: () => { console.log('% button clicked'); }
         },
-        '÷': { 
-          class: 'operationButton', 
-          function: () => { console.log('÷ button clicked'); } 
+        '÷': {
+            class: 'operationButton',
+            function: () => { console.log('÷ button clicked'); }
         },
-        '7': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '7')); } 
+        '7': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '7')); }
         },
-        '8': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '8')); } 
+        '8': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '8')); }
         },
-        '9': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '9')); } 
+        '9': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '9')); }
         },
-        'x': { 
-          class: 'operationButton', 
-          function: () => { console.log('x button clicked'); } 
+        'x': {
+            class: 'operationButton',
+            function: () => { console.log('x button clicked'); }
         },
-        '4': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '4')); } 
+        '4': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '4')); }
         },
-        '5': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '5')); } 
+        '5': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '5')); }
         },
-        '6': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '6')); } 
+        '6': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '6')); }
         },
-        '-': { 
-          class: 'operationButton', 
-          function: () => { console.log('- button clicked'); } 
+        '-': {
+            class: 'operationButton',
+            function: () => { console.log('- button clicked'); }
         },
-        '1': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '1')); } 
+        '1': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '1')); }
         },
-        '2': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '2')); } 
+        '2': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '2')); }
         },
-        '3': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '3')); } 
+        '3': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '3')); }
         },
-        '+': { 
-          class: 'operationButton', 
-          function: () => { console.log('+ button clicked'); } 
+        '+': {
+            class: 'operationButton',
+            function: () => { console.log('+ button clicked'); }
         },
-        '0': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '0')); } 
+        '0': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '0')); }
         },
-        '.': { 
-          class: 'numberButton', 
-          function: () => { result(appendNumber(num1, '.')); } 
+        '.': {
+            class: 'numberButton',
+            function: () => { result(appendNumber(num1, '.')); }
         },
-        '=': { 
-          class: 'operationButton', 
-          function: () => { console.log('= button clicked'); } 
+        '=': {
+            class: 'operationButton',
+            function: () => { console.log('= button clicked'); }
         },
-      };      
+
+        'DEL': {
+            class: 'options',
+            function: () => { console.log('DEL button clicked'); }
+        },
+
+        '^': {
+            class: 'operationButton',
+            function: () => { console.log('^ button clicked'); }
+        },
+    };
 
     return (
-        <button 
-            className= {buttonConfig[content].class}
-            onClick={ buttonConfig[content].function }> {content} </button>
+        <button
+            className={buttonConfig[content].class}
+            onClick={buttonConfig[content].function}> {content} </button>
     )
 }
 
--- src/views/Calculator.jsx
+++ src/views/Calculator.jsx
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from "react";
 import CalcButton from "@components/CalcButton.jsx";
-import { lengthIsCorrect, 
-    isNegative, 
-    isNumber, 
-    plusMinus, 
-    appendNumber, 
-    isOperator, 
-    add, 
-    subtract, 
-    multiply, 
-    divide } from '@scripts/calcController.js';
+import {
+    lengthIsCorrect,
+    isNegative,
+    isNumber,
+    plusMinus,
+    appendNumber,
+    isOperator,
+    add,
+    subtract,
+    multiply,
+    divide
+} from '@scripts/calcController.js';
 
 function Calculator() {
     const [firstNumber, setFirstNumber] = useState("0");
     const [secondNumber, setSecondNumber] = useState("0");
 
-    useEffect (() => {
+    useEffect(() => {
 
-        if(!lengthIsCorrect(firstNumber)){
-            setFirstNumber( firstNumber.substring(0, firstNumber.length - 1))
+        if (!lengthIsCorrect(firstNumber)) {
+            setFirstNumber(firstNumber.substring(0, firstNumber.length - 1))
             alert("Total length must be 9 or less. This includes decimal point")
         }
 
@@ -30,24 +32,34 @@ function Calculator() {
                 {firstNumber}
             </div>
             <div className="keyboard">
-                <CalcButton content={'C'} type={'option'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'+'} type={'operation'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'+/-'} type={'option'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'-'} type={'operation'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'x'} type={'operation'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'÷'} type={'operation'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'7'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'8'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'9'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'4'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'5'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'6'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'1'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'2'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'3'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'.'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'0'} type={'number'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
-                <CalcButton content={'='} type={'operation'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+
+                <CalcButton content={'C'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'+/-'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'DEL'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'^'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+
+                <CalcButton content={'7'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'8'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'9'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'+'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+
+                <CalcButton content={'4'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'5'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'6'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'-'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+
+
+                <CalcButton content={'1'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'2'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'3'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'x'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+
+                <CalcButton content={'='} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'0'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'.'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+                <CalcButton content={'÷'} result={setFirstNumber} num1={firstNumber} num2={secondNumber} />
+
+
             </div>
         </div>
     );
