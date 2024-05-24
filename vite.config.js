ec0e4e982e6cf943e812e546604ef981a259892d
chuy-zip <andreschuy03@gmail.com>
2024-05-24 10:16:11 -0600

    initial config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve : {
+    alias: {
+      '@assets':'/src/assets',
+      '@components':'/src/components',
+      '@styles':'/src/styles',
+      '@views':'/src/views'
+    }
+  },
 })
