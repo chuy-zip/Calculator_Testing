b33949d7ad656442548cf4599d5371ae5fff3610
chuy-zip <andreschuy03@gmail.com>
2024-05-25 11:59:26 -0600

    result is now shown correctly and dinamically

--- src/views/Calculator.jsx
+++ src/views/Calculator.jsx
@@ -20,65 +20,23 @@ function Calculator() {
     const [firstNumber, setFirstNumber] = useState("0");
     const [operation, setOperation] = useState("");
     const [secondNumber, setSecondNumber] = useState("");
+    const [result, setResult] = useState("");
 
     const handleNumberClick = (number) => {
-        if (operation === "" && lengthIsCorrect(firstNumber+".")) {
+        if (operation === "" && lengthIsCorrect(firstNumber + ".")) {
             setFirstNumber((prev) => appendNumber(prev, number));
-            
-        } else if(operation !== "" && lengthIsCorrect(secondNumber+".")){
+        } else if (operation !== "" && lengthIsCorrect(secondNumber + ".")) {
             setSecondNumber((prev) => appendNumber(prev, number));
-        }
-        else {
-            alert("Input is already at 9 characters")
+        } else {
+            alert("Input is already at 9 characters");
         }
     };
 
     const handleOperationClick = (op) => {
-        if (op === "=") {
-
-            if (!(isNumber(firstNumber) && isNumber(secondNumber))) {
-                alert("An invalid input was found")
-                resetCalculator()
-                return;
-            }
-
-            if (firstNumber && secondNumber && operation) {
-                let res;
-                switch (operation) {
-                    case "+":
-                        res = add(firstNumber, secondNumber);
-                        break;
-                    case "-":
-                        res = subtract(firstNumber, secondNumber);
-                        break;
-                    case "x":
-                        res = multiply(firstNumber, secondNumber);
-                        break;
-                    case "÷":
-                        res = divide(firstNumber, secondNumber);
-                        break;
-                    case "^":
-                        res = power(firstNumber, secondNumber);
-                        break;
-                    default:
-                        res = firstNumber;
-                }
-
-                if (!lengthIsCorrect("" + res)) {
-                    resetCalculator()
-                    alert("The result has more than 9 characters. Try other numbers.");
-                    
-                }
-                else if (isNegative(res)) {
-                    resetCalculator()
-                    alert("An error has occured, The result was negative")
-                    
-                } else {
-                    resetCalculator();
-                    setFirstNumber("" + res);
-                    setSecondNumber("");
-                    setOperation("");
-                }
+        if (op === "=" || (isOperator(op) && firstNumber && secondNumber)) {
+            performCalculation();
+            if (op !== "=") {
+                setOperation(op);
             }
         } else if (op === "C") {
             resetCalculator();
@@ -89,15 +47,57 @@ function Calculator() {
                 setSecondNumber((prev) => deleteLast(prev));
             }
         } else if (op === "+/-") {
-            if ((operation === "" && lengthIsCorrect(firstNumber+".")) || (operation === "" && isNegative(firstNumber)) ) {
+            if ((operation === "" && lengthIsCorrect(firstNumber + ".")) || (operation === "" && isNegative(firstNumber))) {
                 setFirstNumber((prev) => plusMinus(prev));
-            } else if(operation !== "" && lengthIsCorrect(firstNumber+".") ||  (operation === "" && isNegative(secondNumber)) ) {
-                setSecondNumber((prev) => plusMinus(prev)); 
+            } else if (operation !== "" && lengthIsCorrect(secondNumber + ".") || (operation !== "" && isNegative(secondNumber))) {
+                setSecondNumber((prev) => plusMinus(prev));
             } else {
-                alert("Input is already at 9 characters")
+                alert("Input is already at 9 characters");
             }
         } else {
             setOperation(op);
+        }
+    };
+
+    const performCalculation = () => {
+        if (!(isNumber(firstNumber) && isNumber(secondNumber))) {
+            alert("An invalid input was found");
+            resetCalculator();
+            return;
+        }
+
+        if (firstNumber && secondNumber && operation) {
+            let res;
+            switch (operation) {
+                case "+":
+                    res = add(firstNumber, secondNumber);
+                    break;
+                case "-":
+                    res = subtract(firstNumber, secondNumber);
+                    break;
+                case "x":
+                    res = multiply(firstNumber, secondNumber);
+                    break;
+                case "÷":
+                    res = divide(firstNumber, secondNumber);
+                    break;
+                default:
+                    res = firstNumber;
+            }
+
+            if (!lengthIsCorrect("" + res)) {
+                resetCalculator();
+                alert("The result has more than 9 characters. Try other numbers.");
+            } else if (isNegative(res)) {
+                resetCalculator();
+                alert("An error has occurred, The result was negative");
+            } else {
+                setResult("" + res);
+                setFirstNumber("" + res);
+                setSecondNumber("");
+                setOperation("");
+                
+            }
         }
     };
 
@@ -105,12 +105,17 @@ function Calculator() {
         setFirstNumber("0");
         setSecondNumber("");
         setOperation("");
+        setResult("");
     };
 
     return (
         <div className="calculatorContainer">
             <div className="operationScreen">
-                {secondNumber || operation ? secondNumber : firstNumber}
+                {
+                    (secondNumber || operation) ? ( result && !secondNumber) ?  
+                        result :
+                        secondNumber:
+                        firstNumber}
             </div>
             <div className="keyboard">
                 {["C", "+/-", "DEL", "^", "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "x", "=", "0", ".", "÷"].map(content => (
